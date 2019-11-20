8561e416e4b08b5f168938705430d85303799d04
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-11-19 22:19:48 -0600

    (Add): Header

---
+++ src/sections/components/header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+function Header({children}) {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.AndroidSafeArea}>
+        <View style={styles.container}>
+          <Image
+            source={require('../../../assets/logo.png')}
+            style={styles.logo}
+          />
+          <View style={styles.right}>{children}</View>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 26,
+    resizeMode: 'contain',
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+});
+
+export default Header;
